3bd8f229ae60a567b223c6ec2834cc786a188800
Tamiiv <tamiis1996.tv@gmail.com>
2025-04-07 17:24:12 -0300

    Configurando js con gulp

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,11 +1,18 @@
-import {src, dest, watch} from 'gulp'
+// series permite ejecutar múltiples tareas
+import {src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass);
 
+export function js(done) {
+    src('src/js/app.js')
+    .pipe( dest('build/js'))
+    done()
+}
+
 // sourcemaps para saber en que archivo de sass se encuentra el código para que lo modifiques
-export function css(done){
+export function css(done) {
     src('src/scss/app.scss', {sourcemaps: true})
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('build/css'), {sourcemaps: true})
@@ -13,7 +20,10 @@ export function css(done){
     done()
 }
 
-export function dev(){
+export function dev() {
     // Va a buscar todos los archivos que tenga la extensión scss dentro de la carpeta scss
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.jd', js)
 }
+
+export default series(js, css, dev)
